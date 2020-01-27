667315b5bce45b115308963a85b5f2c69284f6e9
Christopher Santana <christopher.santana@nav.no>
2020-01-27 16:38:05 +0100

    Debug IV

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ createDevButton() {
 
 createProdButton() {
   echo $( jq -n -c \
-          --arg url "$DEPLOY_PROXY_URL/deploy/dev/$REPOSITORY_PARAM/$GITHUB_SHA" \
+          --arg url "$INPUT_DEPLOY_PROXY_URL/deploy/prod/$REPOSITORY_PARAM/$GITHUB_SHA" \
           '{ type: "button", text: { type: "plain_text", text: "Prod" }, url: $url, style: "danger" }' \
   )
 }
@@ -20,7 +20,7 @@ toArray() {
 }
 
 
-# Show prod-deploy button only for master branch by default. Can be overridden by setting ALLOW_PROD
+# Show prod-deploy button only for master branch by default. Can be overridden by setting INPUT_ALLOW_PROD
 if [[ -z "$INPUT_ALLOW_PROD" ]]; then
   [ "$GITHUB_REF" = "master" ] && INPUT_ALLOW_PROD=true || INPUT_ALLOW_PROD=false
 fi
@@ -38,7 +38,7 @@ for dev_env in ${INPUT_PREPROD_ENVIRONMENTS}; do
   BUTTONS+=($(createDevButton $dev_env))
 done
 
-if $ALLOW_PROD; then
+if $INPUT_ALLOW_PROD; then
   BUTTONS+=($(createProdButton))
 fi
 
@@ -53,7 +53,5 @@ SLACK_PAYLOAD_BASE=$(jq -n -c \
 SLACK_PAYLOAD=$(echo $SLACK_PAYLOAD_BASE | jq -c '.blocks[1].elements = '"$(toArray ${BUTTONS[@]})")
 
 
-echo $SLACK_PAYLOAD
-
 # Post message to slack webook endpoint
-#curl -X POST --data-urlencode "payload=$SLACK_PAYLOAD" $INPUT_WEBHOOK_URL
+curl -X POST --data-urlencode "payload=$SLACK_PAYLOAD" $INPUT_WEBHOOK_URL
