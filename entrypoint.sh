385c554001977e837167cc1a8f01ab54b87e0746
Christopher Santana <christopher.santana@nav.no>
2020-01-27 16:08:29 +0100

    Prefikset input-variabler med 'INPUT'.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 createDevButton() {
   echo $( jq -n -c \
           --arg txt $(printf '%s\n' "$1" | awk '{print toupper($0) }') \
-          --arg url "$DEPLOY_PROXY_URL/deploy/dev/$REPOSITORY_PARAM/$GITHUB_SHA/$1" \
+          --arg url "$INPUT_DEPLOY_PROXY_URL/deploy/dev/$REPOSITORY_PARAM/$GITHUB_SHA/$1" \
           '{ type: "button", text: { type: "plain_text", text: $txt }, url: $url }' \
   )
 }
@@ -21,8 +21,8 @@ toArray() {
 
 
 # Show prod-deploy button only for master branch by default. Can be overridden by setting ALLOW_PROD
-if [[ -z ${ALLOW_PROD+x} ]]; then
-  [ $GITHUB_REF = "master" ] && ALLOW_PROD=true || ALLOW_PROD=false
+if [[ -z ${INPUT_ALLOW_PROD+x} ]]; then
+  [ $GITHUB_REF = "master" ] && INPUT_ALLOW_PROD=true || INPUT_ALLOW_PROD=false
 fi
 
 # Convenience variables
@@ -34,7 +34,7 @@ SHORT_REPO=$(echo $GITHUB_REPOSITORY | rev | cut -f1 -d"/" | rev )
 # Add link buttons for slack message
 BUTTONS=()
 
-for dev_env in ${PREPROD_ENVIRONMENTS}; do
+for dev_env in ${INPUT_PREPROD_ENVIRONMENTS}; do
   BUTTONS+=($(createDevButton $dev_env))
 done
 
@@ -45,7 +45,7 @@ fi
 
 # Create slack message payload
 SLACK_PAYLOAD_BASE=$(jq -n -c \
-                --arg chn "$SLACK_CHANNEL" \
+                --arg chn "$INPUT_SLACK_CHANNEL" \
                 --arg usr "GH Actions Deploy - $SHORT_REPO" \
                 --arg msg "Deploy commit \`$SHORT_SHA\`" \
                         '{ channel: $chn, username: $usr, text: "Deploy-knapp", icon_emoji: ":git:", blocks: [ { type: "section", text: { type: "mrkdwn", text: $msg } },  { type: "actions", elements: [] } ] }' )
@@ -54,4 +54,4 @@ SLACK_PAYLOAD=$(echo $SLACK_PAYLOAD_BASE | jq -c '.blocks[1].elements = '"$(toAr
 
 
 # Post message to slack webook endpoint
-curl -X POST --data-urlencode "payload=$SLACK_PAYLOAD" $WEBHOOK_URL
+curl -X POST --data-urlencode "payload=$SLACK_PAYLOAD" $INPUT_WEBHOOK_URL
